3fefe79cac8a5dad971e83b97b748f92f4f4781f
Brad Jones <brad@jones.name>
2015-10-04 13:50:56 -0600

    Unzip from subdirectory

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 composer create-project drupal-composer/drupal-project:8.x-dev $1 --stability dev --no-interaction
 cd $1
 wget https://github.com/BradJonesLLC/docker-drupal/archive/master.zip
-unzip master.zip
+unzip master.zip "docker-drupal-master/*"
 rm install.sh
 rm master.zip
